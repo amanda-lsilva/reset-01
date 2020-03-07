75b29ad24b7539475e403b4421e25b33b952df53
Reset <Reset@NB-004248.cwinet.local>
2020-03-07 15:47:07 -0300

    alteração calculadora

--- src/App.java
+++ src/App.java
@@ -5,15 +5,15 @@ public class App {
         Calculadora calculadoraDeIdades = new Calculadora();
 
         int somaDasIdades = calculadoraDeIdades.somar(33,54);
-        System.out.println(somaDasIdades);
+        System.out.println( "Soma das Idades = " + somaDasIdades);
 
         int subtracaoDasIdades = calculadoraDeIdades.subtrair(54,33);
-        System.out.println(subtracaoDasIdades);
+        System.out.println( "Subtração das Idades = " + subtracaoDasIdades);
 
         int multiplicacaoDasIdades = calculadoraDeIdades.multiplicar(10, 15);
-        System.out.println(multiplicacaoDasIdades);
+        System.out.println( "Multiplicação das Idades = " + multiplicacaoDasIdades);
 
         int divisaoDasIdades = calculadoraDeIdades.dividir(100, 10);
-        System.out.println(divisaoDasIdades);
+        System.out.println( "Divisão das Idades = " + divisaoDasIdades);
     }
 }

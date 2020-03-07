e80c10d21f199c2e6a620b8aed0c68c59a4f1530
Reset <Reset@NB-004248.cwinet.local>
2020-03-07 15:39:31 -0300

    calculadora

--- src/App.java
+++ src/App.java
@@ -1,6 +1,19 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Calculadora calculadoraDeIdades = new Calculadora();
+
+        int somaDasIdades = calculadoraDeIdades.somar(33,54);
+        System.out.println(somaDasIdades);
+
+        int subtracaoDasIdades = calculadoraDeIdades.subtrair(54,33);
+        System.out.println(subtracaoDasIdades);
+
+        int multiplicacaoDasIdades = calculadoraDeIdades.multiplicar(10, 15);
+        System.out.println(multiplicacaoDasIdades);
+
+        int divisaoDasIdades = calculadoraDeIdades.dividir(100, 10);
+        System.out.println(divisaoDasIdades);
     }
 }

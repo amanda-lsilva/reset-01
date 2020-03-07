ab149e4ec3b40d77730dcea111d02fd26c5024f1
Reset <Reset@NB-004248.cwinet.local>
2020-03-07 12:11:09 -0300

    reset

--- src/App.java
+++ src/App.java
@@ -2,7 +2,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("outra mensagem");
-        System.out.println("novo teste git");
     }
 }

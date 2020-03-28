ddf5fc92924a1bc2bef6ce99bf489bf2456eeee6
amanda-lsilva <amanda.lsilva2@gmail.com>
2020-03-28 16:51:32 -0300

    aula04

---
+++ aula04/src/main/java/br/com/reset/aula04/rest/Usuario.java
@@ -0,0 +1,45 @@
+package br.com.reset.aula04.rest;
+
+public class Usuario {
+
+    private int id;
+    private String nome;
+    private int idade;
+
+    public Usuario(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
